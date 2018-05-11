152f051f7f8b6e8a72df63ca2b0658115ff2969e
linJ-000 <569910495@qq.com>
2018-05-11 21:28:06 +0800

    2.2条件和循环

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import san from 'san'
 import Hello from './component/Hello.san'
 import DataOper from './component/DataOper.san'
+import CondAndCirc from './component/CondAndCirc.san';
 
 import './assets/css/index.css'
 
 
-var dataOper = new DataOper();
-dataOper.attach(document.body);
+// var dataOper = new DataOper();
+// dataOper.attach(document.body);
+
+var condAndCirc  = new CondAndCirc()
+condAndCirc.attach(document.body)
