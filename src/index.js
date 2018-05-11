22283c257347bdd2e10848264939c16af7261e3c
linJ-000 <569910495@qq.com>
2018-05-11 21:51:27 +0800

    2.3样式控制

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import san from 'san'
 import Hello from './component/Hello.san'
 import DataOper from './component/DataOper.san'
-import CondAndCirc from './component/CondAndCirc.san';
+import CondAndCirc from './component/CondAndCirc.san'
+import StyleCtrl from './component/StyleCtrl.san'
 
 import './assets/css/index.css'
 
 
-// var dataOper = new DataOper();
-// dataOper.attach(document.body);
+var dataOper = new DataOper();
+dataOper.attach(document.body);
 
 var condAndCirc  = new CondAndCirc()
 condAndCirc.attach(document.body)
+
+var styleCtrl = new StyleCtrl()
+styleCtrl.attach(document.body)
