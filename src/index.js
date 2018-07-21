bc9e99d8c36253220a5120097751efa9e96358cd
linJ-000 <569910495@qq.com>
2018-07-21 21:43:44 +0800

    我也不知道改了什么

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import san from 'san'
-import Hello from './component/Hello.san'
-import DataOper from './component/DataOper.san'
-import CondAndCirc from './component/CondAndCirc.san'
-import StyleCtrl from './component/StyleCtrl.san'
-import EventHandle from './component/EventHandle.san'
+import App from './App.san'
 
 import './assets/css/index.css'
 
 
-var dataOper = new DataOper();
-dataOper.attach(document.body);
-
-var condAndCirc  = new CondAndCirc()
-condAndCirc.attach(document.body)
-
-var styleCtrl = new StyleCtrl()
-styleCtrl.attach(document.body)
-
-var eventHandle = new EventHandle()
-eventHandle.attach(document.body)
+var app = new App();
+app.attach(document.body);
